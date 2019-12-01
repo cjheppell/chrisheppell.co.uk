090cf98a26e4af1f6f30415b08141b82c7ba5dc5
cjheppell <chris.john.heppell@gmail.com>
2019-12-01 14:35:41 +0000

    wrap the blog lists in container for padding

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,7 +2,7 @@ import React from "react"
 import { useStaticQuery, Link, graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { Grid, Header } from "semantic-ui-react"
+import { Grid, Header, Container } from "semantic-ui-react"
 
 const BlogIndex = () => {
     const data = useStaticQuery(graphql`
@@ -35,38 +35,40 @@ query {
     return (
       <Layout title={siteTitle}>
         <SEO title="All posts" />
-        <Grid>
-            {posts.map(({ node }) => {
-                const title = node.frontmatter.title || node.fields.slug
-                return (
-                    <Grid.Row>
-                        <Grid key={node.fields.slug}>
-                            <Grid.Row stretched>
-                                <Grid.Column>
-                                    <Header as='h3' style={{'margin-bottom': 0}}>
-                                        <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
-                                            {title}
-                                        </Link>
-                                    </Header>
-                                    <p>
-                                        {node.frontmatter.date}
-                                    </p>
-                                </Grid.Column>
-                            </Grid.Row>
-                            <Grid.Row>
-                                <Grid.Column>
-                                    <p
-                                    dangerouslySetInnerHTML={{
-                                        __html: node.frontmatter.description || node.excerpt,
-                                    }}
-                                    />
-                                </Grid.Column>
-                            </Grid.Row>
-                        </Grid>
-                    </Grid.Row>
-                )
-            })}
-        </Grid>
+        <Container>
+            <Grid>
+                {posts.map(({ node }) => {
+                    const title = node.frontmatter.title || node.fields.slug
+                    return (
+                        <Grid.Row>
+                            <Grid key={node.fields.slug}>
+                                <Grid.Row stretched>
+                                    <Grid.Column>
+                                        <Header as='h3' style={{'margin-bottom': 0}}>
+                                            <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
+                                                {title}
+                                            </Link>
+                                        </Header>
+                                        <p>
+                                            {node.frontmatter.date}
+                                        </p>
+                                    </Grid.Column>
+                                </Grid.Row>
+                                <Grid.Row>
+                                    <Grid.Column>
+                                        <p
+                                        dangerouslySetInnerHTML={{
+                                            __html: node.frontmatter.description || node.excerpt,
+                                        }}
+                                        />
+                                    </Grid.Column>
+                                </Grid.Row>
+                            </Grid>
+                        </Grid.Row>
+                    )
+                })}
+            </Grid>
+        </Container>
       </Layout>
     )
 }
