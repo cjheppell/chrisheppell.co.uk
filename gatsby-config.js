edaed30deb45323425ce4aae8e5a6cdc19bb9c69
cjheppell <chris.john.heppell@gmail.com>
2019-12-01 09:57:01 +0000

    configure semantic ui for gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Chris Heppell | Software Engineer`,
+    description: `Personal website for Chris Heppell - Softwre Engineer.`,
+    author: `Chris Heppell`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
